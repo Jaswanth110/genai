f35737084786140a20d553f382308c613c32b41f
jaswanth <143422906+Jaswanth110@users.noreply.github.com>
2024-08-24 15:25:07 +0530

    Add files via upload

---
+++ geminigpt.py
@@ -0,0 +1,14 @@
+import os
+import google.generativeai as genai
+import streamlit as st
+os.environ["GOOGLE_API_KEY"]="AIzaSyCaV6Jjaqvz7PIYg6owNo3Rf0OBbAFbmEw"
+#configure
+genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+model = genai.GenerativeModel("gemini-1.5-pro")
+def generate(input_text):
+    response=model.generate_content(input_text)
+    return response.text
+st.title("Gemini gpt")
+input_text=st.text_input("write your prompt")
+if input_text:
+    st.write(generate(input_text))
---
+++ googlelens.py
@@ -0,0 +1,19 @@
+import os
+import google.generativeai as genai
+import streamlit as st
+from PIL import Image
+os.environ["GOOGLE_API_KEY"]="AIzaSyCaV6Jjaqvz7PIYg6owNo3Rf0OBbAFbmEw"
+#configure
+genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+model = genai.GenerativeModel("gemini-1.5-pro")
+def generate(image,prompt):
+    response=model.generate_content([image,prompt])
+    return response.text
+st.title("lens")
+image=st.file_uploader("upload the image",type=["png","jpeg"])
+input_text=st.text_input("enter the prompt for your image")
+button=st.button("click for result")
+if image and input_text and button:
+    im=Image.open(image)
+    st.image(im)
+    st.write(generate(im,input_text))
